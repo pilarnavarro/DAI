7060223571b6bdfae0cf9859dd72c0e1c0f15889
Pilar <pilarnavar@hotmail.es>
2020-10-20 15:28:12 +0200

    Basic structure

--- p2/app/app.py
+++ p2/app/app.py
@@ -1,6 +1,33 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
           
+import ejercicios as ejers
+
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def welcome():
+  return 'This is the welcome page!'
+
+@app.route('/ordena_matriz')
+def ordena():
+  return ejers.ordena()
+
+@app.route('/criba_eratostenes')
+def criba():
+  return ejers.criba()
+
+@app.route('/fibonacci')
+def fibonacci():
+  return ejers.fibonacci()
+
+@app.route('/balanceo_corchetes')
+def balanceado():
+  return ejers.balanceado()
+
+@app.route('/expresion_regular/<int:index>')
+def regex(index):
+  return ejers.regex(index)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
---
+++ p2/app/ejercicios.py
@@ -0,0 +1,31 @@
+#Ejercicio2
+
+def ordena():
+    return None
+
+
+
+#Ejercicio3
+
+def criba():
+    return None
+
+
+#Ejercicio4
+
+def fibonacci():
+    return None
+
+
+
+#Ejercicio5
+
+def balancedo():
+    return None
+
+
+#Ejercicio6
+
+def regex(index):
+    return None
+
