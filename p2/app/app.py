d33e38463d2312316828ab01d06672a74c72c433
Pilar <pilarnavar@hotmail.es>
2020-10-21 17:49:25 +0200

    Ejercicios p1 terminados

--- p2/app/app.py
+++ p2/app/app.py
@@ -7,25 +7,38 @@ import ejercicios as ejers
 def welcome():
   return 'This is the welcome page!'
 
-@app.route('/ordena_matriz')
-def ordena():
-  return ejers.ordena()
+#Algorithm es burbuja o merge_sort (también merge)
+@app.route('/ordena_matriz/<algorithm>/<lista>')
+def ordena(lista,algorithm):
+  lista=list(map(int,lista.split(",")))
+  return ejers.ordena(lista,algorithm)
 
-@app.route('/criba_eratostenes')
-def criba():
-  return ejers.criba()
+@app.route('/criba_eratostenes/<int:number>')
+def criba(number):
+  message="Los números primos encontrados son:<br/> "
+  return message + ejers.criba(number)
 
-@app.route('/fibonacci')
-def fibonacci():
-  return ejers.fibonacci()
+@app.route('/fibonacci/<path:input>/<path:output>')
+def fibonacci(input,output):
+  message="El número de la sucesión de Fibonacci es:<br/> "
+  end="<br/> Resultado visible en " + output
+  return message + ejers.fibonacci(input,output) + end
 
-@app.route('/balanceo_corchetes')
-def balanceado():
-  return ejers.balanceado()
+@app.route('/balanceo_corchetes', defaults={"cadena": None})
+@app.route('/balanceo_corchetes/<cadena>')
+def balanceado(cadena):
+    if cadena==None:
+        cadena="aleatoria" 
+    
+    if ejers.balanceado(cadena):         
+      return "La cadena " + cadena + " está balanceada"
+    else:
+      return "La cadena " + cadena + " no está balanceada"
 
-@app.route('/expresion_regular/<int:index>')
-def regex(index):
-  return ejers.regex(index)
+@app.route('/expresion_regular/<int:index>/<cadena>')
+def regex(index,cadena):
+  message="Las subcadenas encontradas con el patrón especificado son:<br/>"
+  return message + ejers.regex(index,cadena)
 
 
 @app.errorhandler(404)
